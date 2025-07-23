2eadab808d3076edafa3a314f3daf6c8612bbb9a
lkh3 <lkh3@williams.edu>
2025-07-23 18:39:08 +0000

    hi

--- greekproj/first_app/views.py
+++ greekproj/first_app/views.py
@@ -354,9 +354,10 @@ def nouns(request):
 	else:
 		print("noun on unit: ", noun_on_unit)
 		print("hello")
-		#global units_available
+		global units_available
 		if units_available == []:
 			units_available = ["unit_1"]
+		global nouns_available
 		if nouns_available == []:
 			nouns_available = ["τέχνη"]
 		if on_units == True:
